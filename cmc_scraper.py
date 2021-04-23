2b5a15fa3308f04fd458fe9d558e71dda2449c5e
TysonWu <44836901+tysonwu@users.noreply.github.com>
2021-04-24 05:07:46 +0800

    Clean code
    Clean code

--- cmc_scraper.py
+++ cmc_scraper.py
@@ -22,21 +22,16 @@ def scrape_cmc(date, top=None):
     header = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     r = requests.get(url=url, headers=header)
-    time.sleep(7) # rest for awhile
+    time.sleep(5) # rest for awhile
     soup = BeautifulSoup(r.content, 'lxml')
     soup_rows = soup.findAll('tr', {'class':'cmc-table-row'})
-    # colnames = [col.text for col in soup.findAll(
-    #     'div', {'class':'cmc-table__table-wrapper-outer'}
-    #     )[0].findAll('th') if col.text!='']
-    # print(colnames)
     soup_list = []
     for row in soup_rows:
         soup_list.append([td.text for td in row.findAll('td') if td.text != ''])
     
-    # convert into dataframe
     colnames = ['rank','coin','symbol','market_cap','price','supply','volume_24h','pct_1h','pct_24h','pct_7d']
     df = pd.DataFrame(soup_list, columns=colnames)
-    # print(df)
+
     # clean dataframe
     for col in ['market_cap','price','volume_24h']:
         # print(col)
@@ -60,9 +55,6 @@ def scrape_cmc(date, top=None):
         df = df[df['rank'] <= top]
 
     # output
-    if not os.path.exists(OUTPUT_PATH):
-        os.makedirs(OUTPUT_PATH)
-    
     df.to_csv(f'{OUTPUT_PATH}/cmc_snapshot_{date}.csv', index=False)
     print(df.head())
     print(f'Saved snapshot to output - {date}')
@@ -87,6 +79,9 @@ if __name__ == '__main__':
     start_dt = datetime.strptime(start_date, '%Y%m%d')
     end_dt = datetime.strptime(end_date, '%Y%m%d')
     delta = end_dt - start_dt
+
+    if not os.path.exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
 
     for dt in range(delta.days+1):
         snapshot = datetime.strftime(start_dt + timedelta(days=dt),'%Y%m%d')
